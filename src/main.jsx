605c67408a3119f7ff1a06ba55fdefff58041dc1
chonsawat <chonsawat.nakanam@kkumail.com>
2023-04-29 19:46:43 +0700

    redux-presist

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-/* eslint-disable no-undef */
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
 import { CartProvider } from "./contexts/cart.context";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 
 import "./index.scss";
 
@@ -15,11 +15,13 @@ const rootElement = document.getElementById("root");
 render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   rootElement
