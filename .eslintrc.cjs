d2e969822068a3f0e2337cee346e536f82781061
chonsawat <chonsawat.nakanam@kkumail.com>
2023-04-29 16:23:59 +0700

    config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  env: { browser: false, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
