5944c4054b4062bd0892563b0ed3f33327ffc45a
chonsawat <chonsawat.nakanam@kkumail.com>
2023-05-02 23:22:41 +0700

    bugFixed getAuthCurrentUser on firebase

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -97,7 +97,7 @@ export const createUserDocumentFromAuth = async (
     }
   }
 
-  return userDocRef;
+  return userSnapshot;
 };
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
